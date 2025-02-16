de93f0f02102e6aa5af154790cad5bf8f24b47df
태규 <123963462+EOTAEGYU@users.noreply.github.com>
2025-02-16 15:27:04 +0900

    feat: websocket 이미지 올린 후 도커 컴포즈에서 정상 실행

--- consumer/consumer/src/main/java/org/example/consumer/controller/StockTradeConsumer.java
+++ consumer/consumer/src/main/java/org/example/consumer/controller/StockTradeConsumer.java
@@ -16,33 +16,16 @@ import java.math.BigDecimal;
 public class StockTradeConsumer {
     private final ObjectMapper objectMapper = new ObjectMapper(); // Jackson ObjectMapper 생성
     private final RestTemplate restTemplate = new RestTemplate();
-    private static final String WEBSOCKET_SERVER_URL = "http://localhost:8080/api/trade/send"; // WebSocket 서버 REST API
+    private static final String WEBSOCKET_SERVER_URL = "http://websocket-server:8080/api/trade/send"; // WebSocket 서버 REST API
 
     @Value("${KAFKA_CONSUMER_GROUP:default-group}")
     private String kafkaConsumerGroup;
 
     @KafkaListener(topics = "stock_trade", groupId = "#{T(java.lang.System).getenv('KAFKA_CONSUMER_GROUP') ?: 'default-group'}")
     public void consumeStockTrade(ConsumerRecord<String, String> record) {
-        try {
-            // JSON 문자열을 JsonNode 객체로 변환
-            JsonNode jsonNode = objectMapper.readTree(record.value());
+        log.info("Kafka 메시지 수신: {}", record.value());
 
-            // price와 timestamp 값 추출
-            BigDecimal price = jsonNode.get("price").decimalValue();
-            String timestamp = jsonNode.get("timestamp").asText();
-
-            // JSON 형식으로 웹소켓 클라이언트에 전송할 메시지 생성
-            String jsonMessage = String.format("{\"price\": \"%s\", \"timestamp\": \"%s\"}",
-                    price.toPlainString(), timestamp);
-
-            // 웹소켓을 통해 데이터 전송
-            restTemplate.postForObject(WEBSOCKET_SERVER_URL, jsonMessage, String.class);
-
-            // 로그 출력
-            log.info("체결가={}, 체결시간={}", price.toPlainString(), timestamp);
-
-        } catch (Exception e) {
-            log.error("JSON 파싱 중 오류 발생: {}", e.getMessage());
-        }
+        //웹소켓 서버로 통신
+        restTemplate.postForObject(WEBSOCKET_SERVER_URL, record.value(), String.class);
     }
 }
